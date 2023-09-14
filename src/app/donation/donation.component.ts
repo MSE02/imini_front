67222129aeab4ac2f34d7715551f681c1bd0d21a
MSE02 <128069378+MSE02@users.noreply.github.com>
2023-09-14 20:10:13 +0100

    donation form fixed

--- src/app/donation/donation.component.ts
+++ src/app/donation/donation.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // Importez ces modules
 
 @Component({
   selector: 'app-donation',
@@ -6,6 +7,23 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./donation.component.css'],
 })
 export class DonationComponent {
+  donationForm: FormGroup; // Définissez votre formulaire ici
+
+  constructor(private formBuilder: FormBuilder) {
+    // Créez le formulaire dans le constructeur
+    this.donationForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      amount: ['', [Validators.required, Validators.min(1)]],
+    });
+  }
+
+  onSubmit() {
+    // Logique de soumission du formulaire ici
+    alert('Formulaire soumis !');
+  }
+
   isScreenSizeLarge = window.innerWidth > 992;
 
   @HostListener('window:resize', ['$event'])
